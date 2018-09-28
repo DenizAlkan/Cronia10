01eeea74456988d9ddf3c86be7eb5f9c4a460c5c
fatihgokcee <hmzkts@gmail.com>
2018-09-29 00:12:47 +0300

    SQLite demosu olarak;
    -DatabaseHelper (db main olarak düşünülebilir) oluşturuldu.
    -Veri select ve insert için button backendlerini içeren EditDataActivity classı ve layoutu oluşturuldu.
    -Veri görüntülemek için liste backend classı ve layoutu oluşturuldu. (ListDataActivity)
    -Yukarıdaki işlemler Register.java ve register.xml'in içene dolduruldu.

--- app/src/main/java/com/app/cronia/cronia10/Database/DatabaseHelper.java
+++ app/src/main/java/com/app/cronia/cronia10/Database/DatabaseHelper.java
@@ -1,29 +1,114 @@
 package com.app.cronia.cronia10.Database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by User on 2/28/2017.
+ */
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public DatabaseHelper(Context c) {
-        super(c , "CroniaDB",null,1);
+    private static final String TAG = "DatabaseHelper";
+
+    private static final String TABLE_NAME = "Users";
+    private static final String COL1 = "ID";
+    private static final String COL2 = "name";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, TABLE_NAME, null, 1);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
-        String sql = "create table Users (USER_ID int primary key autoincrement"+
-                ",USER_NAME nvarchar(30) not null"+
-                ",MAIL nvarchar(50) not null" +
-                ",PASSWORD nvarchar(20) not null"+
-                ",REGISTER_DATE datetime default current_timestamp"+
-                ",USER_TYPE_ID tinyint not null"+
-                ",STATUS_ID tinyint)";
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL2 +" TEXT)";
+        db.execSQL(createTable);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int eskiV, int yeniV) {
-
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
+
+    public boolean addData(String item) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, item);
+
+        Log.d(TAG, "addData: Adding " + item + " to " + TABLE_NAME);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        //if date as inserted incorrectly it will return -1
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Returns all the data from database
+     * @return
+     */
+    public Cursor getData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    /**
+     * Returns only the ID that matches the name passed in
+     * @param name
+     * @return
+     */
+    public Cursor getItemID(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT " + COL1 + " FROM " + TABLE_NAME +
+                " WHERE " + COL2 + " = '" + name + "'";
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    /**
+     * Updates the name field
+     * @param newName
+     * @param id
+     * @param oldName
+     */
+    public void updateName(String newName, int id, String oldName){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "UPDATE " + TABLE_NAME + " SET " + COL2 +
+                " = '" + newName + "' WHERE " + COL1 + " = '" + id + "'" +
+                " AND " + COL2 + " = '" + oldName + "'";
+        Log.d(TAG, "updateName: query: " + query);
+        Log.d(TAG, "updateName: Setting name to " + newName);
+        db.execSQL(query);
+    }
+
+    /**
+     * Delete from database
+     * @param id
+     * @param name
+     */
+    public void deleteName(int id, String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "DELETE FROM " + TABLE_NAME + " WHERE "
+                + COL1 + " = '" + id + "'" +
+                " AND " + COL2 + " = '" + name + "'";
+        Log.d(TAG, "deleteName: query: " + query);
+        Log.d(TAG, "deleteName: Deleting " + name + " from database.");
+        db.execSQL(query);
+    }
+
 }
+
--- app/src/main/java/com/app/cronia/cronia10/Register.java
+++ app/src/main/java/com/app/cronia/cronia10/Register.java
@@ -1,14 +1,72 @@
 package com.app.cronia.cronia10;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Register extends AppCompatActivity{
+import com.app.cronia.cronia10.Database.DatabaseHelper;
+import com.app.cronia.cronia10.Database.ListDataActivity;
+
+public class Register extends AppCompatActivity {
+
+    private static final String TAG = "Register";
+
+    DatabaseHelper mDatabaseHelper;
+    private Button buttonAdd, buttonView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        editText = (EditText) findViewById(R.id.editText);
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonView = (Button) findViewById(R.id.buttonView);
+        mDatabaseHelper = new DatabaseHelper(this);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = editText.getText().toString();
+                if (editText.length() != 0) {
+                    AddData(newEntry);
+                    editText.setText("");
+                } else {
+                    toastMessage("You must put something in the text field!");
+                }
+
+            }
+        });
+
+        buttonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, ListDataActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
+    public void AddData(String newEntry) {
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+
+        if (insertData) {
+            toastMessage("Data Successfully Inserted!");
+        } else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
 }
