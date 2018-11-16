f429d4b5754d08ff69648002e826de33ca1ac065
Ömercan Dinç <omercandinc4152@hotmail.com>
2018-11-17 01:11:31 +0300

    dashboard kpi ve listview

---
+++ app/src/main/java/com/app/cronia/cronia10/Home.java
@@ -0,0 +1,11 @@
+package com.app.cronia.cronia10;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+
+}
